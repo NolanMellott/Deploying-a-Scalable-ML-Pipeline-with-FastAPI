05405a165ae3d0bffc2b0a1ac21abf605a6517fb
nmello7 <nmello7@wgu.edu>
2025-09-18 14:14:12 -0400

    Completed TODOs in local_api.py and main.py.

--- main.py
+++ main.py
@@ -26,28 +26,28 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
+path = "model/encoder.pkl"
 encoder = load_model(path)
 
-path = None # TODO: enter the path for the saved model 
+path = "model/model.pkl"
 model = load_model(path)
 
-# TODO: create a RESTful API using FastAPI
-app = None # your code here
 
-# TODO: create a GET on the root giving a welcome message
+app = FastAPI() # your code here
+
+
 @app.get("/")
 async def get_root():
     """ Say hello!"""
-    # your code here
+    return {"greetings": "Hello World!"}
     pass
 
 
-# TODO: create a POST on a different path that does model inference
+
 @app.post("/data/")
 async def post_inference(data: Data):
     # DO NOT MODIFY: turn the Pydantic model into a dict.
-    data_dict = data.dict()
+    data_dict = data.model_dump()
     # DO NOT MODIFY: clean up the dict to turn it into a Pandas DataFrame.
     # The data has names with hyphens and Python does not allow those as variable names.
     # Here it uses the functionality of FastAPI/Pydantic/etc to deal with this.
@@ -65,10 +65,15 @@ async def post_inference(data: Data):
         "native-country",
     ]
     data_processed, _, _, _ = process_data(
+        X=data,
+        categorical_features=cat_features,
+        training=False,
+        encoder=encoder
+
         # your code here
         # use data as data input
         # use training = False
         # do not need to pass lb as input
     )
-    _inference = None # your code here to predict the result using data_processed
+    _inference = inference(model, data_processed) # your code here to predict the result using data_processed
     return {"result": apply_label(_inference)}
