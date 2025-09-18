d13292056f0a83cbffb4a54a69956abec19dfd6e
nmello7 <nmello7@wgu.edu>
2025-09-18 09:56:44 -0400

    Completed TODOs in model.py.

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,7 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
 from ml.data import process_data
-# TODO: add necessary import
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -19,7 +19,12 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
+    RFC = RandomForestClassifier(
+        n_estimators=200,
+        max_depth=40
+    )
+    model = RFC.fit(X_train, y_train)
+    return model
     pass
 
 
@@ -50,7 +55,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : Random forest classifier model
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -59,7 +64,8 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
+    preds = model.predict(X)
+    return preds
     pass
 
 def save_model(model, path):
@@ -72,12 +78,27 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
+    with open(path,'wb') as f:
+        pickle.dump(model, f)
+    print(f"Model saved to {path}")
     pass
 
 def load_model(path):
-    """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
+    """ Loads pickle file from `path` and returns it.
+
+    Inputs
+    ------
+    path:
+        Path to the pickle file containing the saved model.
+    Returns
+    -------
+    model:
+        Saved model.
+    """
+    with open(path, 'rb') as f:
+        model = pickle.load(f)
+    print(f"Loading model from {path}")
+    return model
     pass
 
 
@@ -107,7 +128,7 @@ def performance_on_categorical_slice(
         Trained sklearn OneHotEncoder, only used if training=False.
     lb : sklearn.preprocessing._label.LabelBinarizer
         Trained sklearn LabelBinarizer, only used if training=False.
-    model : ???
+    model : RandomForestClassifier
         Model used for the task.
 
     Returns
@@ -117,12 +138,17 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
     X_slice, y_slice, _, _ = process_data(
+        X = data[data[column_name]==slice_value],
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice) # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
